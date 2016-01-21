c78b34f69c7bc299207a8a375440163154f110a7
Jason Fleagle <jasonfleagle@hotmail.com>
2016-01-21 11:23:34 +0000

    Added jQuery folder and file

---
+++ jQuery/Geolocation.js
@@ -0,0 +1,6 @@
+ if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+      $("#data").html("latitude: " + position.coords.latitude + " <br>longitude: " + position.coords.longitude);
+    });
+  }
+  
